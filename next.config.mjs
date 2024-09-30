c616ff8391ac1895b42fc988fc28cd4b8f2f88cd
VuFeg <ngduogtruongvu@gmail.com>
2024-09-30 14:22:06 +0700

    feat: init

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+	output: 'export',
+	distDir: 'dist',
+	images: {
+		unoptimized: true,
+	},
+}
+module.exports = nextConfig
